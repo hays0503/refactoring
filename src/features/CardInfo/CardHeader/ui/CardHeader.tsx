54b35bd93e0a19918f91caffc36a65a9433d42c9
hays0503 <cthtufgbv@mail.ru>
2024-08-07 19:48:32 +0500

    Fix key error

--- src/features/CardInfo/CardHeader/ui/CardHeader.tsx
+++ src/features/CardInfo/CardHeader/ui/CardHeader.tsx
@@ -20,8 +20,9 @@ const CardHeader = ({ product }: { product: ProductsDetail | null }) => {
         {/* <div className={style.Tag} style={{ backgroundColor: 'rgb(244, 128, 18)' }}>0-0-24</div>
       <div className={style.Tag} style={{ backgroundColor: 'rgb(115, 190, 111)' }}>-19%</div> */}
         <Flex gap="4px 0" wrap >
-          {product?.tag_prod.map((tag) => (
+          {product?.tag_prod.map((tag,index) => (
             <Tag
+              key={index}
               color={tag.fill_color}
               style={{
                 color: `${
