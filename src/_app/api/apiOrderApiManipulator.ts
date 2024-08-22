189cfde970e3ee0cfed2e455e3728a41bac11be1
hays0503 <cthtufgbv@mail.ru>
2024-08-22 14:12:01 +0500

    fix

--- src/_app/api/apiOrderApiManipulator.ts
+++ src/_app/api/apiOrderApiManipulator.ts
@@ -2,7 +2,7 @@ import IOrder from "@/shared/types/order";
 
 export default class OrderApiManipulator {
   public static create(Order: IOrder): Promise<boolean> {
-    const result = fetch("http://pimenov.kz:8989/basket_api/v1/order/", {
+    const result = fetch("/basket_api/v1/order/", {
       headers: {
         accept: "application/json;charset=utf-8",
         'Content-Type': 'application/json;charset=utf-8',
